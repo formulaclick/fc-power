c76d6e04ed54a8a6834ba8acde82c0899e1ac95c
Formula Click <info@formulaclick.com>
2016-05-12 13:27:51 +0200

    CAmbio version

--- fc-power.php
+++ fc-power.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: FC Power
  * Description: Plugin con funciones y seteos personalizados
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Formula Click
  * Author URI: http://www.formulaclick.com
  * License: GPL2
