e4a7e346f493070e75e1dd58f655271e6bf522b5
Formula Click <info@formulaclick.com>
2016-05-12 17:02:00 +0200

    Cambiar updates

--- fc-power.php
+++ fc-power.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: FC Power
  * Description: Plugin con funciones y seteos personalizados
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: Formula Click
  * Author URI: http://www.formulaclick.com
  * License: GPL2
@@ -25,7 +25,6 @@ if ( ! defined( 'WPINC' ) ) {
 require_once( plugin_dir_path( __FILE__ ) . 'functions.php' );
 
 if  ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) {
-
 	require_once( plugin_dir_path( __FILE__ ) . 'plugin-list.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'lib/class-fc-power.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'lib/class-tgm-plugin-activation.php' );
@@ -33,7 +32,5 @@ if  ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'modules/opciones-generales/class-fc-opciones-generales.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'modules/envios-smtp/class-fc-envios-smtp.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'modules/aviso-legal/class-fc-aviso-legal.php' );
-
 	add_action( 'plugins_loaded', array( 'FCPower', 'get_instance' ) );
-	
 }
